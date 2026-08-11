458392ac3b7dde2b226d675003c332489cf78b38
bistik <erickbianes@gmail.com>
2026-08-11 08:43:03 +0800

    record date if user early exits, add test on break countdown

--- pomo.py
+++ pomo.py
@@ -5,11 +5,11 @@ import sys
 from nava import play
 from typing import Final
 
-POMO_SECONDS: Final = 1*15
-BREAK_SECONDS: Final = 1*5
+POMO_SECONDS: Final = 60*25
+BREAK_SECONDS: Final = 60*5
 DATE_FMT: Final = "%b %d, %Y %H:%M:%S %z"
 
-def clear_last_line():
+def clear_last_line() -> None:
     # Move cursor up 1 line (\033[1A) and clear the line (\033[2K)
     sys.stdout.write('\033[1A\033[2K')
     sys.stdout.flush()
@@ -35,7 +35,7 @@ def countdown_pomo(seconds: int, task: str, count: int, date_fmt=DATE_FMT, play_
 
     return end
 
-# We don't record the dates
+# We don't save the dates on break
 def countdown_break(seconds: int, play_sound=True) -> None:
     while seconds >= 0:
         mins, secs = divmod(seconds, 60)
@@ -48,26 +48,32 @@ def countdown_break(seconds: int, play_sound=True) -> None:
 
 def log_pomo(task: str, pomo_data_file: str) -> None:
     count_pomos = 0
-    while True:
-        data = {"task": task, "start": datetime.now().astimezone().strftime(DATE_FMT)}
-        data["end"] = countdown_pomo(seconds=POMO_SECONDS, task=task, count=count_pomos)
+    try:
+        while True:
+            data = {"task": task, "start": datetime.now().astimezone().strftime(DATE_FMT)}
+            data["end"] = countdown_pomo(seconds=POMO_SECONDS, task=task, count=count_pomos)
+            write_data(data, pomo_data_file)
+            count_pomos += 1
+
+            # 15 minutes break on every 3rd consecutive pomo
+            if count_pomos > 0 and count_pomos % 3 == 0:
+                countdown_break(BREAK_SECONDS * 3)
+            else:
+                countdown_break(BREAK_SECONDS)
+
+            if prompt_repeat() == False:
+                break
+    except KeyboardInterrupt as err:
+        # log the date if user ctrl+c
+        data["end"] = datetime.now().astimezone().strftime(DATE_FMT)
         write_data(data, pomo_data_file)
-        count_pomos += 1
-
-        # 15 minutes break on every 3rd consecutive pomo
-        if count_pomos > 0 and count_pomos % 3 == 0:
-            countdown_break(BREAK_SECONDS * 3)
-        else:
-            countdown_break(BREAK_SECONDS)
-
-        if prompt_repeat() == False:
-            break
+        raise err
 
 def write_data(data: dict, pomo_data_file: str) -> None:
     with open(pomo_data_file, "a") as file:
         json.dump(data, file)
         file.write("\n")
 
-def create_pomo_data_file(file_path):
+def create_pomo_data_file(file_path) -> None:
     with open(file_path, "a") as f:
         pass
--- test_pomo.py
+++ test_pomo.py
@@ -1,5 +1,7 @@
+import io
 import unittest
-from pomo import countdown_pomo
+from contextlib import redirect_stdout
+from pomo import countdown_pomo, countdown_break
 from datetime import datetime, timedelta
 
 class TestCrawl(unittest.TestCase):
@@ -8,3 +10,13 @@ class TestCrawl(unittest.TestCase):
         expected = datetime.now().astimezone() + timedelta(seconds=5)
         actual = datetime.strptime(countdown_pomo(5, 'test', 0, play_sound=False), fmt)
         self.assertEqual(actual.strftime(fmt), expected.strftime(fmt), f"actual {actual}")
+
+    def test_countdown_break(self):
+        f = io.StringIO()
+
+        with redirect_stdout(f):
+            countdown_break(3, play_sound=False)
+
+        output = f.getvalue().strip()
+
+        self.assertStartsWith('Break time', output)
